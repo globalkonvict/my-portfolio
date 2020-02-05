d4768387c5d396d3860575e25687c00283d1b02c
Sarthak Dwivedi <sarthakwarlock@gmail.com>
2020-02-05 08:20:19 +0530

    Fixed Padding & Margin Causing Issues on Mobile Devices

--- src/components/About.js
+++ src/components/About.js
@@ -3,20 +3,20 @@ import React from 'react';
 const About = () => {
 	return (
 		<div className="clip-shadow">
-		<section className='me section is-large has-text-centered has-text-white' id='about-me'>
+		<section className='me section has-text-centered has-text-white is-medium' id='about-me'>
 			<div className='container'>
 				<div className='columns is-mobile is-multiline is-centered'>
 					<div className='is-narrow'>
-						<h3 className='is-size-1 is-size-3-mobile about-p section-heading'>About Me!</h3>
+						<h3 className='is-size-1 is-size-4-mobile about-p section-heading'>About Me!</h3>
 					</div>
 					<div className='column is-12'>
-						<p className=' about-p is-size-3 is-size-5-touch has-text-justified has-text-weight-light is-black has-text-left-mobile'>
+						<p className=' about-p is-size-3 is-size-5-touch has-text-justified has-text-weight-light has-text-left-touch'>
 							Hello, I am Sarthak. Currently doing my UG in Electronics and
 							Communications Engineering. I am passionate about programming,
 							learning and staying upto date with the latest trends in
 							technology. Lover of aesthetics, peace and technology, that's all
 							I can say.
-							Want to work together? Reach out to me @sarthakwarlock@gmail.com. 
+							Want to work together? Reach out to me @<span className="is-size-5-mobile" id="email">sarthakwarlock@gmail.com</span>. 
 						</p>
 					</div>
 				</div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
 			<div className='hero-body is-vcentered is-overlay box'>
 				<div className='container '>
 					<h1 className='title has-text-white-bis about-p'>Hello! I am Sarthak.</h1>
-					<h2 className='subtitle has-text-white-bis about-p'>I am an Engineer | Software Developer.</h2>
+					<h2 className='subtitle has-text-white-bis about-p'>I a Software Developer.</h2>
 				</div>
 			</div>			
 		</section>
